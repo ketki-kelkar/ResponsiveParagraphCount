bdf62c368b57cf9ce9c5b5cd421427a3548fd60b
ketki kelkar <ketkikelkar11@gmail.com>
2023-10-14 18:15:07 +0530

    ParaCount

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState,useRef} from 'react';
 
-function App() {
+function App(props) {
+  const textAreaRef = useRef();
+  const [wordCount,setWordCount] = useState(0);
+
+  const countWords = (e) => {
+    const text = textAreaRef.current.value;
+    setWordCount(text.split(" ").length);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className='box'>
+    <center>
+      <br />
+      <h1>Responsive Paragraph Word Counter</h1>
+      <br /><br /><br />
+      <textarea id='cnt' rows='8' cols='40' placeholder='Write a paragraph' ref={textAreaRef} onChange={countWords}></textarea>
+      <br /><br />
+      <h2>Word Count : {wordCount}</h2>
+      <br /><br />
+    </center>
     </div>
+    </>
   );
 }
 
